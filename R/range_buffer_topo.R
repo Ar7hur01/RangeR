9b79fab9ce2b3dfcaef90b414bd4d7e2e9e943f4
Arthur Dreher <arthur.dreher@stud-mail.uni-wuerzburg.de>
2026-03-26 13:22:52 +0100

    added batterlevel to topo_buffer

--- R/range_buffer_topo.R
+++ R/range_buffer_topo.R
@@ -4,9 +4,10 @@
 #' @param lat Numeric latitude.
 #' @param model_name Name des E-Autos (muss in interner DB existieren)
 #' @param batterylevel The Batterylevel (in %) of the vehicle (changes the total range), default = 100 %.
-#' @param z Zoom-Level for elevatr (9-12 recommended)
+#' @param z Zoom-Level for elevatr (6-9 recommended)
 #' @export
-calc_ev_topo_buffer <- function(lon, lat, model_name, start_point, z = 9) {
+
+calc_ev_topo_buffer <- function(lon, lat, model_name, batterylevel = 100, z = 7) {
 
   # Validate Coordinates and transform into sf
   point <- data.frame(lon = lon, lat = lat) |> 
@@ -29,14 +30,37 @@ calc_ev_topo_buffer <- function(lon, lat, model_name, start_point, z = 9) {
     stop(paste0("Model '", model_name, "' not found. Try one of these: ", 
                 paste(head(ev_models_all$model, 3), collapse = ", ")))
   }
+
+  selected_row <- ev_models_all[match_idx[1], ]
+  range_model <- selected_row$range_km[1] * (batterylevel/100) * 1000
+
+  # if batterylevel is not at 100 %
+    if (batterylevel > 100 || batterylevel < 0) {
+    warning("Battery level should be between 0 and 100. Using default (100%).")
+  }
+
+
+
   # 3. DEM Download via elevatr
-  # Wir erstellen eine Bounding Box oder einen Buffer um den Punkt
   message("Lade Höhendaten (DEM) herunter...")
   
   dem <- elevatr::get_elev_raster(
     locations = point, 
-    z = 7,            
+    z = z,            
     clip = "bbox", 
-    expand = 5
+    expand = 50
   )  
+
+  ## Generate the buffer based on the topography and the carmodel's consumption/batterysize
+
+
+
+    map <- leaflet::leaflet() |> 
+    leaflet::addTiles() |>  # Adds default OSM tiles
+    leaflet::addPolygons(data = dem, color = "blue", weight = 2, fillOpacity = 0.3) |> 
+    leaflet::addMarkers(lng = lon, lat = lat, popup = paste0("Model: ", selected_row$model, "<br>",
+                 "Battery: ", batterylevel, "%<br>",
+                 "Est. Range: ", (range_model/1000), " km")
+)
+  return(map)
 }
